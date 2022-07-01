c27a0eaf63250ff281dba92d18a5ab1d30e81265
Cleyton Santana <csttn.dev@gmail.com>
2022-06-30 22:30:05 -0300

    feat: criando database in momory

---
+++ src/app/pages/in-memory-database.ts
@@ -0,0 +1,19 @@
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+import { Observable } from 'rxjs';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb(
+    reqInfo?: RequestInfo | undefined
+  ): {} | Observable<{}> | Promise<{}> {
+    const categories = [
+      { id: 1, name: 'Lazer', description: 'Cinema, parques e etc' },
+      { id: 2, name: 'Moradia', description: 'Aluguel, agua e luz' },
+      { id: 3, name: 'Saúde', description: 'Plano de Saúde e remédios' },
+      { id: 4, name: 'Educação', description: 'Cursos, livros e etc' },
+      { id: 5, name: 'Combustíveis', description: 'Gasolina, álcool e etc' },
+      { id: 6, name: 'Roupas', description: 'Calças, camisas e etc' },
+    ];
+
+    return { categories };
+  }
+}
